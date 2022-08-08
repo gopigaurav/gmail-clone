6205f307c693e4f14be072f2bc1bd53a6e2e1b09
gopi <48626860+gopigaurav@users.noreply.github.com>
2022-08-08 21:37:33 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,6 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWLhkhcRDMt-7tdL75cRE8wkBaTEb9zrk",
-  authDomain: "clone-8c893.firebaseapp.com",
-  projectId: "clone-8c893",
-  storageBucket: "clone-8c893.appspot.com",
-  messagingSenderId: "261322043152",
-  appId: "1:261322043152:web:9073354d10f8efbd8aff95",
-  measurementId: "G-EFW42E6132",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
